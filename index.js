d6b2eb49630f9ded701a0c3a1e6c0c64a34debdd
Andrew Harris <andrew@noblesamurai.com>
2014-02-18 14:57:50 +1100

    use cwd in import path

--- index.js
+++ index.js
@@ -5,12 +5,13 @@ var glob = require('glob');
 module.exports = function(server, sequelize, options) {
   options = options || {};
   options.files = options.files || './models/*.js';
+  var cwd = options.cwd || process.cwd();
 
   // load models
   var models = {};
   glob.sync(options.files, options).forEach(function(file) {
     if (file === 'index.js') return;
-    models[file.slice(0, -3)] = sequelize.import(file);
+    models[file.slice(0, -3)] = sequelize.import(cwd + '/' + file);
   });
 
   // make models available to all requests
